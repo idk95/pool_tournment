68dfdd5d859a4f67fabc70d213395e7511146080
Patricia Jesus <patriciajesus95@hotmail.com>
2020-10-06 10:59:01 +0100

    bug person gone

--- person/main.js
+++ person/main.js
@@ -15,11 +15,11 @@ $(document).ready( function(){
         $.get( "../api/matches/read.php?person_id="+id, function( data ) {
             data.records.forEach(element => {
                 $('#matches tbody').append('<tr>\
-                <td><a href="match/index.html?id='+element.id+'">'+element.date+'</a></td>\
-                <td><a href="person/index.html?id='+element.solids_id+'">'+element.solids_name+'</a></td>\
+                <td><a href="../match/index.html?id='+element.id+'">'+element.date+'</a></td>\
+                <td><a href="../person/index.html?id='+element.solids_id+'">'+element.solids_name+'</a></td>\
                 <td class="text-right">'+element.solids_left+'</td><td>-</td>\
                 <td>'+element.stripes_left+'</td>\
-                <td><a href="person/index.html?id='+element.stripes_id+'">'+element.stripes_name+'</a></td>\
+                <td><a href="../person/index.html?id='+element.stripes_id+'">'+element.stripes_name+'</a></td>\
                 </tr>');
             });
         });
